1e5e85ab5a2bef02d40badc4204157ddf7c2231a
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-12-12 13:44:20 -0300

    Layout NewEventForm implemented

--- components/NewEventForm.js
+++ components/NewEventForm.js
@@ -1,17 +1,43 @@
 import { useState } from 'react';
 
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Col } from 'reactstrap';
 
 const NewEventForm = () => {
-    
-    
+
+
     return (
-    <Form>
-        <FormGroup>
-        <Label for="exampleEmail">Description</Label>
-        <Input plaintext placeholder="Eg.: First day in a new Job" />
-        </FormGroup>
-    </Form>
+        <Form>
+            <FormGroup>
+
+                <div className='mb-4'>
+                    <Label >Description <small>(max 30 chars)</small></Label>
+                    <input type='text' placeholder="Eg.: First day in a new Job" className='form-control' />        
+                </div>              
+
+                <div className='mb-4'>
+                    <Label>Start time</Label>
+                    <Input
+                        type="time"
+                        placeholder="time placeholder"
+
+                    />
+                </div>
+
+                <div className='mb-4'>
+                    <Label>End time</Label>
+                    <Input
+                        type="time"
+                        placeholder="time placeholder"
+                    />
+                </div>
+
+                <div>
+                    <Label>City</Label>
+                    <input type='text' placeholder="Eg.: Quito, Rio de Janeiro, New York" className='form-control' />
+                </div>
+            </FormGroup>
+
+        </Form>
     );
 }
 
