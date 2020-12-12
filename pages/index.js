1f2de6a59c0151b10e8221032183859d94eb42ac
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-12-12 10:59:54 -0300

    Header Calendar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Button } from 'reactstrap';
+import { Container, Table } from 'reactstrap';
 
 export default function Home() {
   return (
@@ -11,8 +11,22 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className='bg-danger'>Calendar</div>
-        <div><Button color="primary">primary</Button></div>
+        <Container>
+          <Table>
+            <thead>
+              <tr className='bg-primary text-white'>
+                <th>Sunday</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+              </tr>
+            </thead>
+
+          </Table>
+        </Container>
       </main>
     </div>
   )
