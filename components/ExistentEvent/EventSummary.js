44f0aeca5af29832f69a0cebfa88ac8e83ef1434
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-12-12 21:06:13 -0300

    Formating desciption in sumary box

--- components/ExistentEvent/EventSummary.js
+++ components/ExistentEvent/EventSummary.js
@@ -1,15 +1,24 @@
 import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
 
+const charBreakPoint = 5;
+
 const EventSummary = (props) => {
-    console.log('props summary: ',props);
+    
+    console.log('props summary: ',props.event);
+    
+    //Unique ID to link the Popover with the sumary box
+    const identifier = (props.event.description + props.event.start + props.event.end + props.event.city ).replace(/[^\w\s]/gi, '')
+    
+    const sizeCurrentDescription = props.event.description.length;
+    const sumaryTextBox = sizeCurrentDescription > charBreakPoint ? `${props.event.description.substring(0, charBreakPoint)}...` : props.event.description; 
 
     return (
         <>
-            <Button id="event-summary" type="button" onClick={(e) => e.stopPropagation()}>
-                {props.event.description}
+            <Button id={identifier} type="button" onClick={(e) => e.stopPropagation()}>
+                {sumaryTextBox}
             </Button>
 
-            <UncontrolledPopover trigger="click" placement="bottom" target="event-summary">
+            <UncontrolledPopover trigger="click" placement="bottom" target={identifier}>
                 <PopoverHeader>{props.event.description}</PopoverHeader>
                 <PopoverBody>
                     <div>
